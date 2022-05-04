1c52bbaf2d26c0203f156e24218a327b813e7d22
A Ahmad <57046949+abbaahmad@users.noreply.github.com>
2022-05-04 20:25:01 +0100

    Update CandidateAssembler.java

--- src/main/java/com/fip/cbt/mapper/model-assembler/CandidateAssembler.java
+++ src/main/java/com/fip/cbt/mapper/model-assembler/CandidateAssembler.java
@@ -1,1 +1,19 @@
+package com.fip.cbt.mapper.modelassemblers;
 
+import com.fip.cbt.controller.api.CandidateController;
+import com.fip.cbt.model.Candidate;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class CandidateAssembler implements RepresentationModelAssembler<Candidate, EntityModel<Candidate>> {
+
+    @Override
+    public EntityModel<Candidate> toModel(Candidate candidate) {
+
+        return EntityModel.of(candidate,
+                linkTo(methodOn(CandidateController.class).getOneCandidate(candidate.getUsername())).withSelfRel());
+    }
+}
