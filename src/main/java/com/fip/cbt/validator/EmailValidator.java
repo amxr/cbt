51439010d4c667958bc8a4ff75f4f13f6741dabe
abbaahmad <abbaahmad50@yahoo.co.uk>
2022-07-03 18:09:17 +0100

    Matching password for sign-up plus accompanying validation for passowrds and emails.

--- src/main/java/com/fip/cbt/validator/EmailValidator.java
+++ src/main/java/com/fip/cbt/validator/EmailValidator.java
@@ -7,8 +7,15 @@ import java.util.regex.Pattern;
 
 public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
     
-    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
-            "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+//    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@" +
+//            "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    
+    // RFC 5322 standard
+    //private static final String EMAIL_PATTERN = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    
+    //Amir's original version
+    private static final String EMAIL_PATTERN = "^(.+)@(\\S+)$";
+    
     private static final Pattern PATTERN = Pattern.compile(EMAIL_PATTERN);
     
     @Override
